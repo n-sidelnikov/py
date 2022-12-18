63d2fa1cfda126f632f09d4bd7dcff21c001b7d6
nikolaysidelnikov <sidelnikovns@tech.bcs.ru>
2022-12-18 23:33:55 +0300

    added sqlite3 connect

--- db/db.py
+++ db/db.py
@@ -1,18 +1,25 @@
 import sqlite3
+from flask import Flask, request, jsonify
 
-db = sqlite3.connect('server.db')
-sql = db.cursor()
+app = Flask(__name__)
+client = app.test_client()
 
+
+def db_con():
+    return sqlite3.connect("server.db", check_same_thread=False)
+
+
+sql = db_con()
 sql.execute("""
-    CREATE TABLE IF NOT EXISTS people(
-     user_id INTEGER,
+    CREATE TABLE IF NOT EXISTS users(
+     user_id INTEGER PRIMARY KEY NOT NULL,
      name VARCHAR,
      surname VARCHAR,
      age INTEGER,
      gender VARCHAR);
     """)
 
-db.commit()
+sql.commit()
 
 user_id = input("ID: ")
 name = input("Name: ")
@@ -20,10 +27,47 @@ surname = input("Surname: ")
 age = input("Age: ")
 gender = input("Gender: ")
 
-sql.execute("SELECT user_id FROM people")
-if sql.fetchone() is None:
-    sql.execute(f"INSERT INTO people VALUES (?, ?, ?, ?, ?)", (user_id, name, surname, age, gender))
-    db.commit()
-    print("Пользователь создан!")
-else:
-    print("Такой пользователь уже есть!")
+sql.execute(f"INSERT INTO users VALUES (?, ?, ?, ?, ?)", (user_id, name, surname, age, gender))
+sql.commit()
+
+
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    response = sql.execute("SELECT * FROM users")
+    query = response.fetchall()
+    return jsonify(query)
+
+
+# if request.method == "POST":
+#     return {
+#         'message': 'This endpoint should create an entity',
+#         'method': request.method,
+#         'body': request.json
+#     }
+
+
+@app.route('/basic_api/entities/<int:entity_id>', methods=['GET', 'PUT', 'DELETE'])
+def entity(entity_id):
+    if request.method == "GET":
+        return {
+            'id': entity_id,
+            'message': 'This endpoint should return the entity {} details'.format(entity_id),
+            'method': request.method
+        }
+    if request.method == "PUT":
+        return {
+            'id': entity_id,
+            'message': 'This endpoint should update the entity {}'.format(entity_id),
+            'method': request.method,
+            'body': request.json
+        }
+    if request.method == "DELETE":
+        return {
+            'id': entity_id,
+            'message': 'This endpoint should delete the entity {}'.format(entity_id),
+            'method': request.method
+        }
+
+
+if __name__ == '__main__':
+    app.run()
